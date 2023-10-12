4ff6b0026ea2a1f5dca67bebf648293f11c0bc8a
Snarloff <snarloff@gmail.com>
2023-10-12 16:22:48 -0300

    feat: :sparkles: adicionado todas as rotas e configurações da OpenAI para o funcionamento ao frontend

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,26 @@
+import { fastifyAutoload } from '@fastify/autoload'
+import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
-import { getAllPromptsRoute } from './routes/get-all-prompts'
+
+import chalk from 'chalk'
+import path from 'path'
 
 const app = fastify()
 
-app.register(getAllPromptsRoute)
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.register(fastifyAutoload, {
+  dir: path.join(__dirname, 'routes'),
+})
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('Server is listening on port http://localhost:3333/')
+    console.log(chalk.bgBlue.white.bold(' Iniciando a API '))
+    console.log(chalk.green.bold('✔ Servidor está ouvindo na porta ') + chalk.yellow.bold('http://localhost:3333/'))
+    console.log(chalk.green.bold('✔ Desenvolvimento por ') + chalk.magenta.bold('Snarloff'))
   })
